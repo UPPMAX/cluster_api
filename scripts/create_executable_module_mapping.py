7c93d9ca255ddc300848aaf1e7a619f5591805e6
Samuel Lampa <samuel.lampa@scilifelab.uu.se>
2013-04-11 18:30:48 +0200

    Started working on python version of the create exec modulefile mapping script

---
+++ scripts/create_executable_module_mapping.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# (c) Samuel Lampa, 2013, UPPMAX
+# Script that creates a file containing a module name on the first column on 
+# each line, followed by all possible binaries of that file, separated by colon.
+import subprocess as s
+import re
+
+def main():
+    mods = {}
+    modfile = open("../etc/modulescache","r")
+    for l in modfile:
+        mod = l.split("/")[0]
+        mods[mod] = []
+
+    print mods
+
+    for modname, mod in mods.iteritems():
+        output = run("/usr/share/Modules/bin/modulecmd bash show " + modname + " 2>&1")
+        print output
+        matches = re.findall("prepend-path.*$", output, re.MULTILINE)
+        print "Matches: " + str(matches)
+
+def run(cmd):
+    print("Now executing command: %s" % cmd)
+    p = s.Popen(cmd, shell=True, stdout=s.PIPE, stderr=s.PIPE)
+    stdout, stderr = p.communicate()
+    if stderr != "":
+        print "ERROR: %s" % stderr
+    return stdout
+
+#function module_bins() {
+#    { for mod in $(cat ../etc/modulescache|grep -v ":"|grep -oP "^[a-zA-Z][a-zA-Z0-9\-\_]*"|grep -v "/"|sort|uniq); do 
+#        echo $mod":"$(module show $mod 2>&1|grep prepend-path|grep -oP "\/[^\:]+"|sort|uniq|xargs ls|tr "\n" ":"); done; 
+#    } 2>/dev/null 
+#}
+#
+#function bin_module_map() {
+#    read line;
+#    mod=$(echo $line|grep -oP "^[^\:]+");
+#    for bin in $(echo $line|tr ":" " "); do
+#        echo "$bin:$mod";
+#    done;
+#}
+#
+#{ for line in $(module_bins); do 
+#    echo $line|bin_module_map;
+#done; } | grep -v bioinfo-tools|grep -v modules|grep -v "\."
+
+def uniq(a_list):
+    return list(set(a_list))
+
+if __name__ == '__main__':
+    main()
