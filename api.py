8ad5f5cf3d8ea9bfc891bd92715e2ddbcc6d57f8
Samuel Lampa <samuel.lampa@gmail.com>
2013-06-13 16:16:27 +0200

    Set exec mode on projects

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 from optparse import OptionParser
+from uppmax import projects
 
 op = OptionParser()
 op.add_option("-e","--endpoint")
+
+for proj in projects.projects_gen():
+        if hasattr(proj, "name"):
+                    print proj.name
+
 
 opts,args = op.parse_args()
 
@@ -9,8 +15,10 @@ if not opts.endpoint:
     print "No endpoint specified! Use the -h flag to view options!"
 
 if opts.endpoint == "projects":
-    print "Projects should be output here ..."
-    # Do something
+    # Output all projects
+    for proj in projects.projects_gen():
+        if hasattr(proj, "name"):
+            print proj.name
 elif opts.endpoint == "persons":
     print "Persons should be output here ..."
     # Do something else
